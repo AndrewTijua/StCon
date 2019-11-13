3f8bb330df0ba5de00d98f25bd7fd1f55d04d51d
Benjamin Cox <ben.10.cox@hotmail.co.uk>
2019-11-13 11:24:59 +0000

    it begins

--- Week9/main.R
+++ Week9/main.R
@@ -1,7 +1,7 @@
 library(fastDummies)
+library(usdm)
 library(data.table)
 library(plyr)
-library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
@@ -16,8 +16,14 @@ library(Hmisc)
 library(doFuture)
 library(aod)
 library(viridis)
+library(ggmosaic)
+library(vcd)
+library(corrplot)
+library(dplyr)
 registerDoFuture()
 plan(multiprocess, workers = availableCores() - 1)
+
+options(datatable.fread.datatable=FALSE)
 
 scale_fill_discrete <- scale_fill_viridis_d
 scale_colour_discrete <- scale_colour_viridis_d
@@ -25,12 +31,53 @@ scale_colour_discrete <- scale_colour_viridis_d
 
 set.seed(1)
 
+df.chisq.test <- function(data, sim = FALSE) {
+  require(plyr)
+  combos <- combn(ncol(data), 2)
+  
+  adply(combos, 2, function(x) {
+    test <-
+      chisq.test(data[, x[1]], data[, x[2]], simulate.p.value = sim)
+    
+    out <- data.frame(
+      "Variable 1" = colnames(data)[x[1]]
+      ,
+      "Variable 2" = colnames(data[x[2]])
+      ,
+      "Chi.Square" = round(test$statistic, 3)
+      ,
+      "df" = test$parameter
+      ,
+      "p.value" = round(test$p.value, 3)
+    )
+    return(out)
+    
+  })[,-1]
+}
+
 passenger_data_raw <- fread(file = "data/titanicMod.csv")
+
 passenger_data_cleaned <-
   select(passenger_data_raw,
-         c(pclass, survived, sex, age, sibsp))
+         c(pclass, survived, sex, age, sibsp, embarked, cabin))
+
 passenger_data_cleaned$pclass <-
   factor(passenger_data_cleaned$pclass)
+
+passenger_data_cleaned$embarked <-
+  factor(passenger_data_cleaned$embarked)
+
+passenger_data_cleaned$sex <- factor(passenger_data_cleaned$sex)
+
+passenger_data_cleaned$cabin[!is.na(passenger_data_cleaned$cabin)] <- 'Y'
+passenger_data_cleaned$cabin[is.na(passenger_data_cleaned$cabin)] <- 'N'
+passenger_data_cleaned$cabin <- factor(passenger_data_cleaned$cabin)
+
+chisq.test(passenger_data_raw$parch, passenger_data_raw$sibsp, simulate.p.value = TRUE)
+chisq.test(passenger_data_cleaned$cabin, passenger_data_cleaned$pclass, simulate.p.value = TRUE)
+chisq.test(passenger_data_cleaned$age, passenger_data_cleaned$embarked, simulate.p.value = TRUE)
+
+df.chisq.test(passenger_data_cleaned, sim = TRUE)
 
 lr_fit <-
   glm(data = passenger_data_cleaned,
@@ -42,6 +89,8 @@ par(mfrow = c(1, 1))
 summary(lr_fit)
 confint(lr_fit)
 
+anova(lr_fit, test = "Chisq")
+
 predicted <-
   round(predict(lr_fit, newdata = passenger_data_cleaned, type = "response"))
 
@@ -49,7 +98,10 @@ predicted <-
 
 confusionMatrix(factor(predicted), factor(passenger_data_cleaned$survived))
 
-
+plot_fit <-
+  glm(data = passenger_data_cleaned,
+      formula = survived ~ age + pclass + sex,
+      family = binomial(link = "logit"))
 
 age_data_m <-
   with(
@@ -57,14 +109,13 @@ age_data_m <-
     data.frame(
       pclass = factor(rep(1:3, each = 81)),
       age = rep(seq(0:80), 3)-1,
-      sex = "male",
-      sibsp = mean(sibsp)
+      sex = "male"
     )
   )
 age_data_pm <-
   cbind(age_data_m,
         predict(
-          lr_fit,
+          plot_fit,
           newdata = age_data_m,
           type = "link",
           se = TRUE
@@ -89,14 +140,13 @@ age_data_f <-
     data.frame(
       pclass = factor(rep(1:3, each = 81)),
       age = rep(seq(0:80), 3)-1,
-      sex = "female",
-      sibsp = mean(sibsp)
+      sex = "female"
     )
   )
 age_data_pf <-
   cbind(age_data_f,
         predict(
-          lr_fit,
+          plot_fit,
           newdata = age_data_f,
           type = "link",
           se = TRUE
