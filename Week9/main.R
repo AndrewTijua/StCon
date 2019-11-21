814e561164cfe01771dc330d010887a6b639c361
Benjamin Cox <ben.10.cox@hotmail.co.uk>
2019-11-21 13:55:51 +0000

    everyone dies

--- Week9/main.R
+++ Week9/main.R
@@ -82,7 +82,7 @@ df.chisq.test(passenger_data_cleaned, sim = TRUE)
 
 lr_fit <-
   glm(data = passenger_data_cleaned,
-      formula = survived ~ .,
+      formula = survived ~ . + age*sex + age*pclass,
       family = binomial(link = "logit"))
 par(mfrow = c(2, 2))
 plot(lr_fit)
@@ -99,9 +99,9 @@ predicted <-
 
 confusionMatrix(factor(predicted), factor(passenger_data_cleaned$survived))
 
-size_bfl <- 0.75
-size_bou <- 0.45
-alp <- 0.25
+size_bfl <- 0.95
+size_bou <- 0.55
+alp <- 0.45
 
 plot_fit <-
   glm(data = passenger_data_cleaned,
@@ -171,3 +171,82 @@ w_plot_ageclass <- ggplot(age_data_pf, aes(x = age, y = PredictedProb)) +
   labs(title = "Predicted Female Survival by Class")
 
 ggarrange(m_plot_ageclass, w_plot_ageclass, common.legend = TRUE)
+
+ggQQ = function(lm, probs = c(0.05,0.95)) {
+  # extract standardized residuals from the fit
+  d <- data.frame(std.resid = rstandard(lm))
+  # calculate 1Q/4Q line
+  y <- quantile(d$std.resid[!is.na(d$std.resid)], probs)
+  x <- qnorm(probs)
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
+  
+  p <- ggplot(data=d, aes(sample=std.resid)) +
+    stat_qq(shape=1, size=3) +           # open circles
+    labs(title="Normal Q-Q",             # plot title
+         x="Theoretical Quantiles",      # x-axis label
+         y="Standardized Residuals") +   # y-axis label
+    geom_abline(slope = slope, intercept = int, linetype="dashed")  # dashed reference line
+  return(p)
+}
+
+ggplot_lm <- function(model,
+                      probs = c(0.05, 0.95),
+                      di = "norm",
+                      de = TRUE) {
+  data <- model$model
+  p1 <- ggplot(mapping = aes(x = .fitted,
+                             y = .resid)) +
+    geom_point(data = model) +
+    xlab("Fitted Values") +
+    ylab("Residuals") +
+    ggtitle("Residuals vs Fitted")
+  
+  p1<-ggplot(model, aes(.fitted, .resid))+geom_point()
+  p1<-p1+stat_smooth(method="loess")+geom_hline(yintercept=0, col="red", linetype="dashed")
+  p1<-p1+xlab("Fitted values")+ylab("Residuals")
+  p1<-p1+ggtitle("Residual vs Fitted Plot")
+  
+  res.df <- data.frame(s.res = rstandard(model))
+  #res.df$y = quantile(res.df$s.res[!is.na(res.df$s.res)], probs)
+  
+  p2 <-
+    ggplot(data = res.df, aes(sample = s.res)) +
+    stat_qq_point(distribution = di,
+                  detrend = de,
+                  qprobs = probs) +
+    stat_qq_line(distribution = di,
+                 detrend = de,
+                 qprobs = probs) +
+    stat_qq_band(distribution = di,
+                 detrend = de,
+                 qprobs = probs) +
+    labs(x = "Theoretical", y = "Standardised \nResiduals", title = "Q-Q Plot for response")
+  
+  #p2 <- ggQQ(model, probs)
+  
+  p3<-ggplot(model, aes(.hat, .stdresid))+geom_point(aes(size=.cooksd), na.rm=TRUE)
+  p3<-p3+stat_smooth(method="loess", na.rm=TRUE)
+  p3<-p3+xlab("Leverage")+ylab("Standardized Residuals")
+  p3<-p3+ggtitle("Residual vs Leverage Plot")
+  p3<-p3+scale_size_continuous("Cook's Distance", range=c(1,5))
+  #p4 <- gg_scalelocation(model)
+  p4<-ggplot(model, aes(seq_along(.cooksd), .cooksd))+geom_bar(stat="identity", position="identity")
+  p4<-p4+xlab("Obs. Number")+ylab("Cook's distance")
+  p4<-p4+ggtitle("Cook's distance")
+  return(ggarrange(p1, p2, p3, p4))
+}
+
+ggplot_lm(model = lr_fit, de = TRUE)
+
+ill_passenger_data <- fread(file = "data/newPassengers.csv")
+
+ill_passenger_data$pclass <-
+  factor(ill_passenger_data$pclass)
+
+ill_passenger_data$embarked <-
+  factor(ill_passenger_data$embarked)
+
+ill_passenger_data$sex <- factor(ill_passenger_data$sex)
+
+ill_passenger_data$p_surv <- (predict(lr_fit, newdata = ill_passenger_data, type = "response"))
